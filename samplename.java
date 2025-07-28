82086ca2de68a7830e31368399fd2d20929a8c46
harshiniadvik <v.harshini1817@gmail.com>
2025-07-28 18:06:35 +0530

    lab7

---
+++ samplename.java
@@ -0,0 +1,9 @@
+public class samplename {
+    public static void main(String[] args) {
+        // Declare a variable to hold the name
+        String name = "Johny";
+        
+        // Print the name to the console
+        System.out.println("Hello, my name is " + name);
+    }
+}
