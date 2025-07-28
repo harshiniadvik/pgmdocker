fc6cac78e3d80b91eeac017cd91a002517cff2c1
harshiniadvik <v.harshini1817@gmail.com>
2025-07-28 18:51:19 +0530

    adding

--- samplename.java
+++ samplename.java
@@ -1,7 +1,7 @@
 public class samplename {
     public static void main(String[] args) {
         // Declare a variable to hold the name
-        String name = "Johny";
+        String name = "advikaadhyasri
         
         // Print the name to the console
         System.out.println("Hello, my name is " + name);
